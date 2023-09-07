0782708ceb80e6b227304d65ffe035e5dabe418a
Angelina <huangelina03@gmail.com>
2023-09-06 18:57:34 -0700

    Feature 16 complete

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_task
+from .views import create_task, list_of_tasks
 
 
 urlpatterns = [
-    path("create/", create_task, name="create_task")
+    path("create/", create_task, name="create_task"),
+    path("mine/", list_of_tasks, name="show_my_tasks")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from tasks.forms import TaskForm
+from tasks.models import Task
 
 
 @login_required
@@ -16,3 +17,12 @@ def create_task(request):
         "form": form,
     }
     return render(request, "projects/form_task.html", context)
+
+
+@login_required
+def list_of_tasks(request):
+    tasks = Task.objects.filter(assignee=request.user)
+    context = {
+        "list_of_tasks": tasks,
+    }
+    return render(request, "tasks/show_tasks.html", context)
