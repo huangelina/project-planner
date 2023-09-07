81b49f43513d303e0f41be7e28b38a79469a6a8b
Angelina <huangelina03@gmail.com>
2023-09-06 19:58:36 -0700

    Feature 17 complete

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, redirect, render
-
 from projects.forms import ProjectForm
 from .models import Project
 from django.contrib.auth.decorators import login_required
