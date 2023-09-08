3acc2613d7b80643f49a06c13586895a66f68432
Angelina <huangelina03@gmail.com>
2023-09-08 15:38:48 -0700

    Final submission

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,5 @@ from .views import create_task, list_of_tasks
 
 urlpatterns = [
     path("create/", create_task, name="create_task"),
-    path("mine/", list_of_tasks, name="show_my_tasks")
+    path("mine/", list_of_tasks, name="show_my_tasks"),
 ]
